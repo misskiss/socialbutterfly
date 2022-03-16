af2e404fa847c97f22a2ab25f32c5f84e85d8bb3
Michelle Kiss <picturethis56@gmail.com>
2022-03-16 11:34:20 -0700

    delete post mutation added

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,4 @@
+const { AuthenticationError } = require('apollo-server');
 const Post = require('../../models/Post');
 const checkAuth = require('../../util/check-auth');
 
@@ -5,7 +6,8 @@ module.exports = {
   Query: {
     async getPosts() {
       try {
-        const posts = await Post.find();
+        // tell mongoose to sort by most recently created
+        const posts = await Post.find().sort({ createdAt: -1 });
         return posts;
       } catch (err) {
         throw new Error(err);
@@ -22,7 +24,7 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
-    },
+    }
   },
   Mutation: {
     async createPost(_, { body }, context) {
@@ -32,11 +34,26 @@ module.exports = {
         body,
         user: user.id,
         username: user.username,
-        createdAt: new Date().toISOString(),
+        createdAt: new Date().toISOString()
       });
       const post = await newPost.save();
 
       return post;
     },
-  },
+    async deletePost(_, { postId }, context) {
+      const user = checkAuth(context);
+
+      try {
+        const post = await Post.findById(postId);
+        if (user.username === post.username) {
+          await post.delete();
+          return 'Post successfully deleted';
+        } else {
+          throw new AuthenticationError('Action cannot be completed');
+        }
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
+  }
 };
